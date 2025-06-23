dc6f691e3bbf2233c0a821f2b6937960402f3560
Jon C <me@jonc.dev>
2025-06-23 13:10:40 -0400

    Clean up repo for clarity (#14)
    #### Problem
    
    As noted at #13, it's a bit confusing to have a `program` folder in this
    repository, when there's no program.
    
    #### Summary of changes
    
    This repo will be used for the interface and generated clients
    eventually, so just make it clear what's in this repo, and remove the
    `program` directory for the moment.
    
    Fixes #13

--- program/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Vote Program.
-
-#[cfg(all(target_os = "solana", feature = "bpf-entrypoint"))]
-mod entrypoint;
-pub mod processor;
-
-solana_program::declare_id!("Vote111111111111111111111111111111111111111");
